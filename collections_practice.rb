48d4572ac40c6efb9c19336eb952b14c79a9826c
Steven Laff <steve.laff@gmail.com>
2019-08-23 02:45:17 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,32 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort |a, b|
+  
+end
+
+def sort_array_char_count(array)
+
+end
+
+def swap_elements
+  
+end
+
+def kesha_maker
+  
+end
+
+def find_a
+  
+end
+
+def sum_array
+  
+end
+
+def add_s
+  
+end
